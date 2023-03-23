67483d5e00305bd7e0582daae42382f3bca24e5f
Thony2 <desarrollo2.punku@gmail.com>
2023-03-22 21:00:21 -0500

    actualizcion de tipado de album

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,10 +4,20 @@ export interface Song{
     title: string
 
  album:Album   
+ artist:Artist
 }
 export interface Album{
     title:string
     cover:string
+    cover_big: string
+    cover_medium: string
+    cover_small: string
+}
+export interface Artist {
+    name: string
+    picture: string
+    picture_big: string
+    picture_medium: string
 }
 declare global {
     interface Window {
