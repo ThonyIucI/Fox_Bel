1cf13f113d66e46787728910007319b533d889db
Thony2 <desarrollo2.punku@gmail.com>
2023-03-22 21:01:50 -0500

    card y funcion de seleccionar

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useDataContext } from '../context/dataContext';
 import '../styles/card.css'
 import { Song } from '../vite-env';
 interface prop{
@@ -6,6 +7,7 @@ interface prop{
 }
 
 const Card: FC<prop> = ({song}) => {
+    const{showSong}=useDataContext()
     const playSong=()=>{
         window.DZ.init({
             appId: '590384',
@@ -23,8 +25,8 @@ const Card: FC<prop> = ({song}) => {
        
     }
     return (
-        <div className="fox-card">
-            <div className="card-image" style={{backgroundImage: `url("${song?.album?.cover}")`}} >
+        <div className="fox-card" onClick={() => showSong(song)}>
+            <div className="card-image" style={{backgroundImage: `url("${song?.album?.cover_medium}")`}} >
                 {/* <img src={`${song?.album?.cover}`} alt={`${song.title}`} /> */}
                 {/* <i className="fa fa-ellipsis-v" aria-hidden="true"></i> */}
                 <i className="fa fa-play" aria-hidden="true" onClick={playSong}></i>
