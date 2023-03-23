4c5b8ca72f61f6ed1d0e7d03b4220d7839ca3579
Thony2 <desarrollo2.punku@gmail.com>
2023-03-22 21:01:06 -0500

    simplificacino de home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 
 import './styles/index.css'
-import Home from './components/drawer/Home'
-import { useDataContext } from './context/dataContext'
-import Card from './components/Card'
 import './styles/app.css'
+import Home from './pages/Home'
 
 
 function App() {
-  const {results}=useDataContext()
+
 
   return (
-     <Home>
-        <div className='cards'>
-        {results && results.length? results?.map(
-          e=><Card song={e} key={e.id} />
-        ):null}
-        </div>
-      </Home>
+    <Home/>
    
   )
 }
